b5305bb7d8b519995d2bd9e0e7d9904d8cf9f770
kingjky <kingjky@gmail.com>
2020-08-10 00:19:54 +0900

    Merge backend

--- back-end/routes/users.js
+++ back-end/routes/users.js
@@ -31,31 +31,4 @@ router.post('/', function(req, res, next) {
     res.send({ status: "OK", data: info });
 });
 
-
-// router.post('/', function(req, res, next) {
-//     const len = users.data.length;
-//     const info = req.body;
-//     users.data.push({
-//         no: (len + 1),
-//         email: `${info.email}`,
-//         password: `${info.password}`,
-//         name: `${info.name}`
-//     });
-//     const stringJson = JSON.stringify(users);
-// fs.open('./public/database/users.json', 'a', "666", function(err, id) {
-//     if (err) {
-//         console.log("file open err!!");
-//     } else {
-//         fs.writeFile('./public/database/users.json', '', function() {
-//             console.log('file is cleand!');
-//             fs.write(id, stringJson, null, 'utf8', function(err) {
-//                 console.log('file was saved!');
-//             });
-//         });
-//     }
-// })
-//     console.dir(info);
-//     res.send({ status: "OK", data: info });
-// });
-
 module.exports = router;
