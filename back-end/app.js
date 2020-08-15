b2e9af8eed519dbae3965b75e73e7216eee2ed5b
domino8788 <domino8788@naver.com>
2020-08-16 00:59:11 +0900

    Mod app.js 이미지 파일 static api 구현

--- back-end/app.js
+++ back-end/app.js
@@ -9,36 +9,39 @@ var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 var signupRouter = require('./routes/signup');
 var guestbookRouter = require('./routes/guestbook');
+var postRouter = require('./routes/post');
 var callRouter = require('./routes/call');
 var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
-app.use(cors())
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
-app.use('/login', loginRouter)
-app.use('/signup', signupRouter)
-app.use('/guestbook', guestbookRouter)
-app.use('/call', callRouter)
-    // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    next(createError(404));
+app.use('/image', express.static('uploads'));
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/login', loginRouter);
+app.use('/signup', signupRouter);
+app.use('/guestbook', guestbookRouter);
+app.use('/call', callRouter);
+app.use('/post', postRouter);
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 // error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-    // render the error page
-    res.status(err.status || 500);
-    // res.render('error');
-    res.send(err.message);
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  // res.render('error');
+  res.send(err.message);
 });
 module.exports = app;
